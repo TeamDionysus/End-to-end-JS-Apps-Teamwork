7d177afb64e3e16a518df1be42982aa1304a2eb8
StefanSinapov <stefan.sinapov@gmail.com>
2014-10-05 16:32:44 +0300

    Application skeleton

---
+++ server/config/mongoose.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Models = require('../models');
+
+module.exports = function(config) {
+    mongoose.connect(config.db);
+    var db = mongoose.connection;
+
+    db.once('open', function(err) {
+        if (err) {
+            console.log('Database could not be opened: ' + err);
+            return;
+        }
+
+        console.log('Database up and running...');
+    });
+
+    db.on('error', function(err){
+        console.log('Database error: ' + err);
+    });
+
+    Models.User.seedInitialUsers();
+    Models.Course.seedInitialCourses();
+};
---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var UserModel = require('../models/user'),
+    CourseModel = require('../models/Course');
+
+module.exports = {
+    User : UserModel,
+    Course : CourseModel
+};
