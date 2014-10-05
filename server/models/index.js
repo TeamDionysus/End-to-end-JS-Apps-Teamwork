39c2d1724574aa5f3aa1a16bb4356eeeddf640a7
neutrino <jrusev@gmail.com>
2014-10-05 23:48:32 +0300

    renamed Course.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var UserModel = require('../models/user'),
-    CourseModel = require('../models/Course');
+    CourseModel = require('../models/course');
 
 module.exports = {
     User : UserModel,
