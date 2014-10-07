d4fffef53859f4399e1c2fcf223c39bf2fdd9c3b
neutrino <jrusev@gmail.com>
2014-10-08 01:49:22 +0300

    changed home page design

--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -2,8 +2,9 @@
 
 'use strict';
 
-app.controller('MainCtrl', function($scope, $rootScope, $location, cachedCourses, itemsData) {
+app.controller('MainCtrl', function($scope, $rootScope, $location, identity, cachedCourses, itemsData) {
     $scope.courses = cachedCourses.query();
+    $scope.identity = identity;
     
     $scope.findItem = function (query) {
         if (query) {
