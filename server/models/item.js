efb6a16ffe7f817ccbccd48594525b5e2457e55e
neutrino <jrusev@gmail.com>
2014-10-08 02:50:47 +0300

    changed seed items

--- server/models/item.js
+++ server/models/item.js
@@ -27,21 +27,21 @@ module.exports.seedInitialCourses = function() {
         }
 
         if (collection.length === 0) {
-            Item.create({title: 'C# for Sociopaths', featured: true, published: new Date('10/5/2013'), categories: ['C#'], price: 20});
-            Item.create({title: 'C# for Non-Sociopaths', featured: true, published: new Date('10/12/2013'), categories: ['C#'], price: 200});
-            Item.create({title: 'Super Duper Expert C#', featured: false, published: new Date('10/1/2013'), categories: ['C#'], price: 250});
-            Item.create({title: 'Visual Basic for Visual Basic Developers', featured: false, published: new Date('7/12/2013'), categories: ['VB'], price: 120});
-            Item.create({title: 'Pedantic C++', featured: true, published: new Date('1/1/2013'), categories: ['C++'], price: 180});
-            Item.create({title: 'JavaScript for People over 20', featured: true, published: new Date('10/13/2013'), categories: ['JS'], price: 230});
-            Item.create({title: 'Maintainable Code for Cowards', featured: true, published: new Date('3/1/2013'), categories: ['Coding'], price: 50});
-            Item.create({title: 'A Survival Guide to Code Reviews', featured: true, published: new Date('2/1/2013'), categories: ['Coding'], price: 350});
-            Item.create({title: 'How to Job Hunt Without Alerting your Boss', featured: true, published: new Date('10/7/2013'), categories: ['Misc'], price: 400});
-            Item.create({title: 'How to Keep your Soul and go into Management', featured: false, published: new Date('8/1/2013'), categories: ['Management'], price: 500});
-            Item.create({title: 'Telling Recruiters to Leave You Alone', featured: false, published: new Date('11/1/2013'), categories: ['Misc'], price: 520});
-            Item.create({title: "Writing Code that Doesn't Suck", featured: true, published: new Date('10/13/2013'), categories: ['Coding'], price: 430});
-            Item.create({title: 'Code Reviews for Jerks', featured: false, published: new Date('10/1/2013'), categories: ['Coding'], price: 380});
-            Item.create({title: 'How to Deal with Narcissistic Coworkers', featured: true, published: new Date('2/15/2013'), categories: ['Misc'], price: 90});
-            Item.create({title: 'Death March Coding for Fun and Profit', featured: true, published: new Date('7/1/2013'), categories: ['Coding', 'Misc'], price: 690});
+            Item.create({title: 'MacBook Air 13"', featured: true, published: new Date('10/5/2013'), categories: ['Computers'], price: 890});
+            Item.create({title: 'iPhone 5S new', featured: true, published: new Date('10/12/2013'), categories: ['Phones'], price: 300});
+            Item.create({title: 'BMX Bycicle', featured: false, published: new Date('10/1/2013'), categories: ['Sports'], price: 150});
+            Item.create({title: 'Luxury Lamp', featured: false, published: new Date('7/12/2013'), categories: ['Furniture'], price: 75});
+            Item.create({title: 'Monet reproduction', featured: true, published: new Date('1/1/2013'), categories: ['Art'], price: 500});
+            Item.create({title: 'Encyclopedia Britanica', featured: true, published: new Date('10/13/2013'), categories: ['Books'], price: 30});
+            Item.create({title: 'Canon 600D', featured: true, published: new Date('3/1/2013'), categories: ['Cameras'], price: 500});
+            Item.create({title: 'Awesome Mix Tape', featured: true, published: new Date('2/1/2013'), categories: ['Music'], price: 50});
+            Item.create({title: 'Top Gun Jacket', featured: true, published: new Date('10/7/2013'), categories: ['Clothing'], price: 200});
+            Item.create({title: 'Seiko Kinnetic 2K', featured: false, published: new Date('8/1/2013'), categories: ['Watches'], price: 400});
+            Item.create({title: 'Woodie', featured: false, published: new Date('11/1/2013'), categories: ['Toys'], price: 20});
+            Item.create({title: 'Sony Vaio', featured: true, published: new Date('10/13/2013'), categories: ['Computers'], price: 1430});
+            Item.create({title: 'Winter tyres', featured: false, published: new Date('10/1/2013'), categories: ['Car-parts'], price: 180});
+            Item.create({title: 'used electric wheelchair', featured: true, published: new Date('2/15/2013'), categories: ['Misc'], price: 90});
+            Item.create({title: 'Baby stroller', featured: true, published: new Date('7/1/2013'), categories: ['Baby-gear'], price: 80});
         }
     });
 };
