51405d041316e883354850a98ed1be92b6cc5abe
izabelk <izi_girl@abv.bg>
2014-10-08 17:33:53 +0300

    added messagesModel, should work on controller, that is now only added

---
+++ server/models/message.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+var mongoose = require('mongoose');
+var Message;
+
+module.exports.init = function () {
+    var messagesSchema = mongoose.Schema({
+        content: { type: String, required: '{PATH} is required' },
+        from : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        to: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    });
+    
+    Message = mongoose.model('Message', messagesSchema);
+};
