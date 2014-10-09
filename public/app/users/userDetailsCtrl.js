32dd8bc41d2ff1a0c53400271415f328907af5c8
StefanSinapov <stefan.sinapov@gmail.com>
2014-10-09 21:15:51 +0300

    user details page and user profile page fixed (was broken)

--- public/app/users/userDetailsCtrl.js
+++ public/app/users/userDetailsCtrl.js
@@ -5,7 +5,13 @@
 app.controller('UserDetailsCtrl', function ($scope, $routeParams, usersData, itemsData, notifier) {
     
     $scope.currentUserId = $routeParams.id;
-    
+    $scope.currentPage = 0;
+    $scope.numPerPage = 10;
+    $scope.maxSize = 5;
+    $scope.user = {};
+    $scope.user.items = [];
+
+
     usersData.getById($scope.currentUserId).then(
         function (user) {
             $scope.user = user;
@@ -23,4 +29,14 @@ app.controller('UserDetailsCtrl', function ($scope, $routeParams, usersData, ite
         notifier.error('Unable to load user from server: ' + error.message || error);
         }
     );
+
+    $scope.numPages = function () {
+        return Math.ceil(($scope.user.items.length || 10) / $scope.numPerPage);
+    };
+
+    $scope.$watch('currentPage + numPerPage', function () {
+        var begin = ($scope.currentPage - 1) * $scope.numPerPage;
+        var end = begin + $scope.numPerPage;
+        $scope.user.filteredItems = $scope.user.items.slice(begin, end);
+    });
 });
