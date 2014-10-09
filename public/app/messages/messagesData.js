11b2f84eb9dfabdc4d4a94d520540d8a0022c0b9
StefanSinapov <stefan.sinapov@gmail.com>
2014-10-10 00:05:44 +0300

    inbox page

--- public/app/messages/messagesData.js
+++ public/app/messages/messagesData.js
@@ -4,11 +4,38 @@
 
 app.factory('messagesData', function messagesData($http, $q, identity) {
 
-    var getInbox = function(){
+    var messagesUrl = 'api/messages';
 
+    var getInbox = function(){
+        var deferred = $q.defer();
+
+        $http.get(messagesUrl + '/inbox')
+            .success(function(response){
+                deferred.resolve(response);
+            })
+            .error(function(error){
+                deferred.reject(error);
+            });
+
+        return deferred.promise;
+    };
+
+    var getSendByMe = function(){
+        var deferred = $q.defer();
+
+        $http.get(messagesUrl + '/sent')
+            .success(function(response){
+                deferred.resolve(response);
+            })
+            .error(function(error){
+                deferred.reject(error);
+            });
+
+        return deferred.promise;
     };
 
     return {
-        getInbox: getInbox
+        getInbox: getInbox,
+        getSendByMe: getSendByMe
     };
 });
