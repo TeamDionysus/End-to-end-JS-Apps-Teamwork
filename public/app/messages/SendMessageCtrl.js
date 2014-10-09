c4e365c660fb04e121e25e8ff104d97953128b5b
izabelk <izi_girl@abv.bg>
2014-10-09 23:56:17 +0300

    added skeleton of message send, post request should be fixed

---
+++ public/app/messages/SendMessageCtrl.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+app.controller('SendMessageCtrl', 
+    function SendMessageCtrl($scope, messagesData, notifier) {
+    
+    $scope.sendMessage = function (message, sendMessageForm) {
+        
+        //console.log(message);
+        
+        if (sendMessageForm.$valid) {
+            messagesData.sendMessage(message)
+            .then(
+            function (success) {
+                notifier.success("Message sent successfully!");
+            //$location.path('/');
+            },
+            function (error) {
+                notifier.error(error.message);
+            });
+        }
+    };
+});
--- public/app/messages/messagesData.js
+++ public/app/messages/messagesData.js
@@ -2,13 +2,33 @@
 
 'use strict';
 
-app.factory('messagesData', function messagesData($http, $q, identity) {
-
-    var getInbox = function(){
+app.factory('messagesData', function messagesData($http, $q, $routeParams, identity) {
+    
+    var getInbox = function () {
 
     };
-
+    
+    var sendMessage = function (newMessage) {
+        var deferred = $q.defer();
+        
+        console.log(newMessage);
+        
+        $http.post('/api/messages/send/' + $routeParams.username, newMessage, {
+            //transformRequest: angular.identity,
+            //headers: { 'Content-Type': undefined }
+        })
+        .success(function (message) {
+            deferred.resolve(message);
+        })
+        .error(function (error) {
+            deferred.reject(error);
+        });
+        
+        return deferred.promise;
+    }
+    
     return {
-        getInbox: getInbox
+        getInbox: getInbox,
+        sendMessage: sendMessage
     };
 });
