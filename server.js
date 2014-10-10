e4cae01e2118d9864cb855b2fae5d743522727db
neutrino <jrusev@gmail.com>
2014-10-10 09:12:02 +0300

    edit server.js env

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 
 var env = process.env.NODE_ENV || 'development';
 console.log('process.env.NODE_ENV : ' + process.env.NODE_ENV)
-console.log('process.env.PORT : ' + process.env.PORT)
 
 var app = express();
 var config = require('./server/config/config')[env];
